fbd3e1c5fb4cef60c7eab58418ed4717ead12d62
league <league@imac.attlocal.net>
2023-03-05 11:30:11 -0800

    Scammer

--- src/_02_Generics_Store/GoShopping.java
+++ src/_02_Generics_Store/GoShopping.java
@@ -3,40 +3,50 @@ package _02_Generics_Store;
 public class GoShopping {
 	// 1. Look through other classes to see how they all interact.
 	//
-	//    There is a generic Cart class that can hold different 
-	//    types of objects.
+	// There is a generic Cart class that can hold different
+	// types of objects.
 	//
-	//    Candy and Cereal are Food objects
-	//    Clothing and Toy are NonFood objects.
-	
+	// Candy and Cereal are Food objects
+	// Clothing and Toy are NonFood objects.
+
 	public static void main(String[] args) {
 		// 2. Create a Cart object of type Candy
-		
+	//	Cart<Candy> cart1 = new Cart<Candy>();
 		// 3. Create another Cart object of type Cereal
-		
+	//	Cart<Cereal> cart2 = new Cart<Cereal>();
 		// 4. Add a few items to EACH cart
-		
-		
-		
-		
+	//	Cereal cereal = new Cereal();
+	//	Candy candy = new Candy();
+	//	cart1.add(candy);
+	//	cart1.add(candy);
+	//	cart2.add(cereal);
+	//	cart2.add(cereal);
 		// 5. Call the showCart() method on EACH cart
-		
-		
+	//	cart1.showCart();
+	//	cart2.showCart();
 		// 6. Run the code
-		
+
 		// 7. Comment out the code you have so far
-		
+
 		// 8. Repeat steps 2-5 for a Clothing Cart and a Toy Cart
-		
-		
-		
-		
+		Cart<Clothing> cart3 = new Cart<Clothing>();
+		Cart<Toy> cart4 = new Cart<Toy>();
+		Clothing clothing = new Clothing();
+		Toy toy = new Toy();
+			cart3.add(clothing);
+			cart3.add(clothing);
+			cart4.add(toy);
+			cart4.add(toy);
+			cart3.showCart();
+			cart4.showCart();
 		// Why are there errors?
-		
+			
+//Compile errors occur because the cart class is a bounded type Generic class and only extends the food class, not the non-food class.
+			
 		// 9. Modify the Cart class so that this code compiles
 		// HINT: There are 3 lines of code to edit
-				
+
 		// 10. Run the code. Does it work?
-		
+//Yes!!
 	}
 }
--- src/_03_Intro_to_Scanners/ScannerDemo.java
+++ src/_03_Intro_to_Scanners/ScannerDemo.java
@@ -4,75 +4,71 @@ import java.util.Scanner;
 
 public class ScannerDemo {
 
-    /*
-     * A Scanner is an alternative to taking user input by using the console.
-     * Unlike a JOptionPane it is also capable of taking in many data types
-     * besides String.
-     */
+	/*
+	 * A Scanner is an alternative to taking user input by using the console. Unlike
+	 * a JOptionPane it is also capable of taking in many data types besides String.
+	 */
 
-    public static void main(String[] args) {
-        /*
-         * 1. Create an object of the Scanner class.
-         * 
-         * Note: You will need to pass System.in to the constructor(no quotes).
-         * 
-         * You might notice this looks similar to System.out which you use for
-         * printing to the console and that's because they are related. These
-         * are what are known as standard input and output streams.
-         * 
-         * In both these cases the standard input and output streams are
-         * Eclipse's console. This may change depending on the environment where
-         * are running your program or the input/output stream you are using.
-         */
+	public static void main(String[] args) {
+		/*
+		 * 1. Create an object of the Scanner class.
+		 *
+		 * Note: You will need to pass System.in to the constructor(no quotes).
+		 * 
+		 * You might notice this looks similar to System.out which you use for printing
+		 * to the console and that's because they are related. These are what are known
+		 * as standard input and output streams.
+		 * 
+		 * In both these cases the standard input and output streams are Eclipse's
+		 * console. This may change depending on the environment where are running your
+		 * program or the input/output stream you are using.
+		 */
+		Scanner scammer = new Scanner(System.in);
 
-        
-        
-        /*
-         * 2. Use a syso to ask the user for their name and then use the Scanner
-         * created in step 1 to take in the user's name as a String.
-         * 
-         * You can use either next() or nextLine(). next() will take in all
-         * string input until it reaches a space. nextLine will take in all
-         * string input until it reaches a newline.
-         * 
-         * You should see a blinking cursor in the console that you can type
-         * into.
-         * 
-         * Note: Don't forget to save the results of next() or nextLine() into a
-         * String variable.
-         */
+		/*
+		 * 2. Use a syso to ask the user for their name and then use the Scanner created
+		 * in step 1 to take in the user's name as a String.
+		 * 
+		 * You can use either next() or nextLine(). next() will take in all string input
+		 * until it reaches a space. nextLine will take in all string input until it
+		 * reaches a newline.
+		 * 
+		 * You should see a blinking cursor in the console that you can type into.
+		 * 
+		 * Note: Don't forget to save the results of next() or nextLine() into a String
+		 * variable.
+		 */
+		System.out.println("Hello friend! What is your name?");
+		String name = scammer.nextLine();
+	//	System.out.println(name);
 
-        
-        
-        /*
-         * 3. Ask the user for their age in the console. Then use the Scanner to
-         * take their age as an int and save it into an int variable.
-         * 
-         * See if you can find the scanner method that does this without relying
-         * on Integer.parseInt().
-         */
+		/*
+		 * 3. Ask the user for their age in the console. Then use the Scanner to take
+		 * their age as an int and save it into an int variable.
+		 * 
+		 * See if you can find the scanner method that does this without relying on
+		 * Integer.parseInt().
+		 */
+System.out.println("Nice to meet you " + name + "! I'm Scammer. How old are you?");
+int age = scammer.nextInt();
+//System.out.println(age);
+		/*
+		 * 4.) Try asking the user for another piece of information that's not a String
+		 * or an int.
+		 * 
+		 * Note: There are many methods similar to the one you used for int that are
+		 * capable of taking in primitive data types.
+		 */
+System.out.println(age + "?! Ehh. you're old enough to scam I guess. Hehe oops, I didn't say anything . . . So friend, what's your parents' credit card number? And their social security number? :)");
+		// 5.) Print the user's data to the console nicely formatted.
 
-        
-        
-        /*
-         * 4.) Try asking the user for another piece of information that's not a
-         * String or an int.
-         * 
-         * Note: There are many methods similar to the one you used for int that
-         * are capable of taking in primitive data types.
-         */
+		/*
+		 * 6.) Close your scanner to avoid memory leaks.
+		 * 
+		 * Note: When using a Scanner with the System.in input stream in larger programs
+		 * only close the scanner when your program is completely done.
+		 */
 
-        // 5.) Print the user's data to the console nicely formatted.
-
-        
-        
-        /*
-         * 6.) Close your scanner to avoid memory leaks.
-         * 
-         * Note: When using a Scanner with the System.in input stream in larger
-         * programs only close the scanner when your program is completely done.
-         */
-
-    }
+	}
 
 }
